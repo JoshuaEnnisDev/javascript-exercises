bf842ee616dc2a30a3170ea12a5985fb6c4a4b35
Josh <joshuaennisdev@gmail.com>
2026-07-27 23:09:18 -0500

    added functions.html and function-scope.html

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -8,8 +8,14 @@
 */
 
 // Add your code right below, good luck!
-
-
+firstName = "Carlos"
+lastName = "Stevenson"
+thisYear = 1965
+birthYear = 1947
+age = 18
+fullName = `${firstName} ${lastName}`
+greeting = `Hello! My name is ${fullName} and I am ${thisYear - birthYear} years old.`
+console.log(greeting)
 
 
 
